f749d34a5054119c94388534e612442d0e29c6aa
Jade Sailor <jgitpublic.45358@sai9.net>
2026-02-08 09:35:31 -0500

    Stop using racket/private/* dependencies of racket/base, part 1
    In `mzscheme/priavte/*.rkt`, there are some uses of some of various
    standard Scheme-ey macros. Previously, those were being imported
    directly from various `racket/private/*` modules which were chunks
    of racket/base. Import them from `racket/base` instead.
    
    Test:
    
    Change was done interactively. The count of bound uses DrRacket shows for
    each of the required modules adds up properly. Checking that the required
    identifiers are compatible is manual.

--- compatibility-lib/mzscheme/private/stxmz-body.rkt
+++ compatibility-lib/mzscheme/private/stxmz-body.rkt
@@ -3,7 +3,7 @@
 
 (module stxmz-body '#%kernel
   ;; These could probably change to just be `(require racket/base)`.
-  (#%require racket/private/define
+  (#%require (only racket/base define-syntax)
              (for-syntax '#%kernel racket/private/stx))
 
   ;; So that expansions print the way the Racket programmer expects:
